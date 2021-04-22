373aacbbf455ee2ce1c8da69e422f22babb6903c
uesleipedro <ielseu@hotmail.com>
2021-04-22 01:00:58 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Image
+} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { color } from 'react-native-reanimated';
+
+import userImg from '../assets/perfil.jpg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header(){
+    return(
+        <View style={style.container}>
+            <View>
+                <Text style={style.greeting}>Olá,</Text>
+                <Text style={style.userName}>Ueslei</Text>
+            </View>
+            <Image source={ userImg } style={style.image}/>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    }
+});
